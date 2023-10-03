c86dcef72d3ca932dcd846f88593cea503f935cb
Carly <88619398+carlyldn@users.noreply.github.com>
2023-10-03 19:09:13 +0100

    burger

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){  
+    $(".burger-button").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").toggleClass("active");
+    });
+    
+    });
+
 // Auto update footer year
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("year-mob").innerHTML = new Date().getFullYear();
